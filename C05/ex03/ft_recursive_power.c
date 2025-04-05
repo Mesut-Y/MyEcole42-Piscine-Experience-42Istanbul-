b0a8959e2400e9d6c6e137aaf7f4ac0f6457797b
Mesut-Y <mesutyanmaz.1@gmail.com>
2025-04-05 23:13:26 +0300

    C05 görevleri yüklendi.

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myanmaz <myanmaz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 19:58:09 by myanmaz           #+#    #+#             */
+/*   Updated: 2024/09/05 17:20:42 by myanmaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power > 0)
+		return (nb * ft_recursive_power(nb, power - 1));
+	return (1);
+}
